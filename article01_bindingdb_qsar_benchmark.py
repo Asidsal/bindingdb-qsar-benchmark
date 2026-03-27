abefdf5e16bc268bf4cfc6b5dd86d83d55ccc1fa
Asidsal <130383171+Asidsal@users.noreply.github.com>
2026-03-27 10:31:05 +0100

    Update article01_bindingdb_qsar_benchmark.py

--- article01_bindingdb_qsar_benchmark.py
+++ article01_bindingdb_qsar_benchmark.py
@@ -5,7 +5,7 @@ Article 01 — Benchmarking Machine Learning Models for Bioactivity Prediction
 ================================================================================
 Authors : Abubakar Siddiq Salihu, Muhammad Sulaiman Rahma,
           Wan Mohd Nuzul Hakim Wan Salleh, Nura Suleiman Gwaram,
-          Sulaiman Sani Yusuf
+          Sulaiman Sani Yusuf, Abdulganiyu Mannir
 Journal : Journal of Chemical Information and Modeling (submitted 2026)
 GitHub  : https://github.com/[YOUR_USERNAME]/bindingdb-qsar-benchmark
 Zenodo  : https://doi.org/[YOUR_DOI]
